186d80dd26b33e32ec35af446831c1e66c429715
Палехов Фёдор <f.palekhov@domain.local>
2025-05-21 13:15:11 +0300

    android_test ++

--- kt3/test_android.py
+++ kt3/test_android.py
@@ -3,6 +3,7 @@ from appium.webdriver.common.appiumby import AppiumBy
 import time
 @pytest.mark.parametrize("browser",["android"],indirect=True)
 def test_android_browser(browser):
-    el = browser.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value='new UiSelector().text("Battery")')
+    el = browser.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value='new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("Battery"))')
     el.click()
     browser.save_screenshot("screen_battery.png")
+
