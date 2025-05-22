f8f1e17b2b9794dffdd859f3034e53c7a723093b
Палехов Фёдор <f.palekhov@domain.local>
2025-05-22 17:53:12 +0300

    +++моб. тестирование

--- kt3/conftest.py
+++ kt3/conftest.py
@@ -25,10 +25,11 @@ capabilities = dict(
     platformName='Android',
     automationName='uiautomator2',
     deviceName='Android',
-    appPackage='com.android.settings',
-    appActivity='.Settings',
-    language='en',
-    locale='US'
+    browserName = 'Chrome',
+    # appPackage='com.android.settings',
+    # appActivity='.Settings',
+    # language='en',
+    # locale='US'
 )
 appium_server_url = 'http://localhost:4723'
 
--- kt3/test_android.py
+++ kt3/test_android.py
@@ -3,7 +3,15 @@ from appium.webdriver.common.appiumby import AppiumBy
 import time
 @pytest.mark.parametrize("browser",["android"],indirect=True)
 def test_android_browser(browser):
-    el = browser.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value='new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("Battery"))')
-    el.click()
-    browser.save_screenshot("screen_battery.png")
+    browser.find_element(AppiumBy) 
+
+
+
+    # el = browser.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value='new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("Battery"))')
+    # el.click()
+    # print(browser.current_activity)
+    # print(browser.current_context)
+    # print(browser.current_package)
+    # browser.activate_app("com.android.settings")
+    # browser.save_screenshot("screen_battery.png")
 
