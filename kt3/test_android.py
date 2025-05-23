007f3a476af3b9bb68d278a4307348a766f1c241
Палехов Фёдор <f.palekhov@domain.local>
2025-05-23 19:41:35 +0300

    ++++моб. тестирование

--- kt3/test_android.py
+++ kt3/test_android.py
@@ -1,12 +1,18 @@
 import pytest
 from appium.webdriver.common.appiumby import AppiumBy
-import time
+
 @pytest.mark.parametrize("browser",["android"],indirect=True)
 def test_android_browser(browser):
-    browser.find_element(AppiumBy) 
 
+    browser.get("https://ogorodnik.by/")
+    browser.implicitly_wait(5)
+    browser.find_elements(AppiumBy.CSS_SELECTOR,"[title='Поиск по каталогу']")[1].send_keys("Перец сладкий Аристотель F1")
+    assert  browser.find_element(AppiumBy.CSS_SELECTOR,".stock.out-of-stock.wd-style-default").text == "Нет в наличии", "Под товаром НЕТ информации, что товвара нет!!!!"
 
-
+    # browser.get("https://ogorodnik.by/")
+    # # print(browser.page_source)
+    # browser.find_element(AppiumBy.TAG_NAME, "TEXTAREA").send_keys("Meow")
+    # browser.find_element(AppiumBy) 
     # el = browser.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value='new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("Battery"))')
     # el.click()
     # print(browser.current_activity)
